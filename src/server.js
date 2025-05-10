86890a96ba95a38e0f5ffa440844c596054b4a36
Anderson Lopez <lopezanderson069@gmail.com>
2025-05-10 02:19:40 -0500

    Fix-file-server.js

--- src/server.js
+++ src/server.js
@@ -12,19 +12,22 @@ dotenv.config();
 const app = express();
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
-
 // Rutas
 app.use('/api/admin', adminRoutes);
 app.use('/api/payments', paymentRoutes);
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+});
 app.get('/', (req, res) => {
   res.send('API de pagos en línea');
 });
 
+const PORT = process.env.PORT || 5000;
 
 // DB + servidor
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB conectado');
-    app.listen(process.env.PORT, () => console.log(`Servidor en puerto ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
   })
   .catch((err) => console.error('Error conectando MongoDB:', err));
